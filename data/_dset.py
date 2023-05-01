a53a2be9c85087ef0d259401e3ad64005c063968
Magenta195 <novelistoblige@gmail.com>
2023-05-01 11:34:53 +0900

    change dataset loading mechanism

--- data/_dset.py
+++ data/_dset.py
@@ -2,19 +2,43 @@ from PIL import Image
 
 from torch.utils.data import Dataset
 
+# class WallPaperDataset(Dataset):
+#     def __init__(self, img_path_list, label_list, transforms=None):
+#         self.img_path_list = img_path_list
+#         self.label_list = label_list
+#         self.transforms = transforms
+        
+#     def __getitem__(self, index):
+#         img_path = self.img_path_list[index]
+        
+#         image = Image.open(img_path)
+        
+#         if self.transforms is not None:
+#             image = self.transforms(image)
+        
+#         if self.label_list is not None:
+#             label = self.label_list[index]
+#             return image, label
+#         else:
+#             return image
+        
+#     def __len__(self):
+#         return len(self.img_path_list)
+
 class WallPaperDataset(Dataset):
     def __init__(self, img_path_list, label_list, transforms=None):
-        self.img_path_list = img_path_list
         self.label_list = label_list
         self.transforms = transforms
+        self.img_list = list()
+        for img_path in img_path_list :
+            image = Image.opne(img_path)
+            if self.transforms is not None:
+                image = self.transforms(image)
+
+            self.img_list.append(image)
         
     def __getitem__(self, index):
-        img_path = self.img_path_list[index]
-        
-        image = Image.open(img_path)
-        
-        if self.transforms is not None:
-            image = self.transforms(image)
+        image = self.img_list[index]
         
         if self.label_list is not None:
             label = self.label_list[index]
@@ -23,4 +47,4 @@ class WallPaperDataset(Dataset):
             return image
         
     def __len__(self):
-        return len(self.img_path_list)
+        return len(self.img_list)
