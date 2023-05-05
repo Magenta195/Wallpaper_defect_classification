b5c6750e7dea643625cd5fb47aa503665f391744
Magenta195 <novelistoblige@gmail.com>
2023-05-05 16:07:46 +0900

    split traininer

--- data/__init__.py
+++ data/__init__.py
@@ -1,7 +1,7 @@
 from typing import List, Dict, Union, Tuple, Optional, Type
 import glob
 import os 
-import unicodedata
+
 
 from sklearn.model_selection import train_test_split, StratifiedKFold
 from torch.utils.data import DataLoader, random_split, Subset
@@ -10,81 +10,11 @@ from torchvision.datasets import ImageFolder
 from .utils import CONFIG
 from ._dset import *
 from ._argumentation import *
+from ._dutils import *
 
 __all__ = [WallPaperDataset]
 
 mode_list = ['train', 'val', 'test']
-
-def get_dataloader(
-        mode : str,
-        cfg : Type[CONFIG],
-        img_path_list : Optional[List[str]] = None,
-        label_list : Optional[List[str]] = None,
-        dataset : Optional[Dataset] = None,
-    ) -> DataLoader :
-    
-    if mode not in mode_list :
-        raise ValueError("Invaild DataLoader Type")
-    
-    if mode == 'train' :
-        transform = train_transforms( cfg = cfg )
-        shuffle = True
-    else :
-        transform = test_transforms( cfg = cfg )
-        shuffle = False
-
-    if dataset is None :
-        dataset = WallPaperDataset(
-            img_path_list = img_path_list,
-            label_list = label_list,
-            transforms = transform
-        )
-
-    return DataLoader(
-        dataset = dataset,
-        batch_size = cfg.BATCH_SIZE,
-        shuffle = shuffle,
-        num_workers = cfg.NUM_WORKER
-    )
-
-def get_data_list(
-        mode : str,
-        cfg : Type[CONFIG],
-    ) -> Union[Tuple[List[str], None], Tuple[List[str], List[str]]] :
-    
-    if mode == 'train' :
-        TRAIN_DATA_PATH = os.path.join(cfg.DATA_PATH, 'train', '*', '*')
-        img_list = glob.glob(TRAIN_DATA_PATH)
-        label_list  = list()
-
-        for img_dir in img_list :
-            label_name = str(img_dir).split('/')[-2]
-            try :
-                label = cfg.CLASS_DICT[label_name]
-            except :
-                label = cfg.CLASS_DICT[unicodedata.normalize('NFC', label_name)]
-            label_list.append(label)
-
-        return img_list, label_list
-
-    else :
-        TEST_DATA_PATH = os.path.join(cfg.DATA_PATH, 'test', 'test', '*')
-        img_list = glob.glob(TEST_DATA_PATH)
-
-        return img_list, None
-    
-
-def get_image_folder(
-    mode : str,
-    cfg : Type[CONFIG],
-) -> ImageFolder:
-    DATA_PATH = os.path.join(cfg.DATA_PATH, mode)
-    if mode == 'train':
-        img_folder = ImageFolder(root=DATA_PATH, transform=train_transforms( cfg = cfg ))   
-    else:
-        img_folder = ImageFolder(root=DATA_PATH, transform=test_transforms( cfg = cfg ))
-
-    return img_folder
 
 def get_all_dataloader(
         cfg : Type[CONFIG],
@@ -141,7 +71,6 @@ def get_kfold_dataloader(
 
     dataloader_dict = dict()
     datalist_dict = dict()
-
     kfold = StratifiedKFold(n_splits = cfg.KFOLD)
     # Only use torchvision.datasets.ImageFolder
   
--- trainer/_trainer.py
+++ trainer/_trainer.py
@@ -126,18 +126,17 @@ class TRAINER() :
             self._train_one_epoch()
             self._val_one_epoch()
             self._save_best_model()
-            if self._early_stopping() :
-                break
-
             if self.scheduler is not None :
                 self.scheduler.step()
 
             print( " [ epoch : {:03d} ] train_loss : {:0.03f}, val_loss : {:0.03f}, val_score : {:0.03f}, max_val_score : {:0.03f} ".format(
-                i+1,
+                i,
                 self.train_loss,
                 self.val_loss,
                 self.cur_score,
                 self.best_score))
+            if self._early_stopping() :
+                break
 
 
     def make_predict( self ) -> np.ndarray :
